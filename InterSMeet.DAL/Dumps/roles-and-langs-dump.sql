a18d4c57d1cd11168c6ab36e0b0ea6ec22d5eb1b
AdriGB <67225211+Zackysh@users.noreply.github.com>
2022-01-10 11:13:48 +0100

    feat(roles & languages migration)

---
+++ InterSMeet.DAL/Dumps/roles-and-langs-dump.sql
@@ -0,0 +1,6 @@
+INSERT INTO user_roles (role_id, name) VALUES(1, 'Admin');
+INSERT INTO user_roles (role_id, name) VALUES(2, 'Company');
+INSERT INTO user_roles (role_id, name) VALUES(3, 'Student');
+
+INSERT INTO languages (language_id, name) VALUES(1, 'en');
+INSERT INTO languages (language_id, name) VALUES(2, 'es');
---
+++ InterSMeet.DAL/Migrations/20220110101240_add-roles-and-langs.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterSMeet.DAL.Migrations
+{
+    public partial class addrolesandlangs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = @"../InterSMeet.DAL/Dumps/roles-and-langs-dump.sql";
+            migrationBuilder.Sql(File.ReadAllText(sql));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
